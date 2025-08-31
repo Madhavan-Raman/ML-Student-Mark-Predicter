189c78e06b8a0f6a4346a97f0831b9323499161e
MADHAVAN R <ramanmadhavan01@gmail.com>
2025-08-31 11:17:26 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import joblib
+
+
+try:
+    model = joblib.load("./model.pkl")
+    print("Model loaded successfully")
+except Exception as e:
+    print(f"Error loading model: {e}")
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to Student Marks Prediction API!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json()
+        hours = data.get("hours")
+
+        if hours is None:
+            return jsonify({"error": "Missing 'hours' in request"}), 400
+
+        prediction = model.predict([[hours]])
+        return jsonify({"hours": hours, "predicted_marks": round(prediction[0], 2)})
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
